e87cf3bafb32cff20fc008074c4b2fd9cd18997a
syssun <2506715686@qq.com>
2019-05-29 21:22:15 +0800

    fragment

--- app/src/main/java/com/sys/MainActivity.java
+++ app/src/main/java/com/sys/MainActivity.java
@@ -3,6 +3,7 @@ package com.sys;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v4.view.ViewPager;
 import android.os.Bundle;
 import android.view.MenuItem;
@@ -18,88 +19,91 @@ import com.sys.fragments.FragmentTwo;
 import java.util.ArrayList;
 
 public class MainActivity extends FragmentActivity {
-    private ViewPager viewPager;
 
     private MenuItem menuItem;
     private BottomNavigationView bottomNavigationView;
-
+    FragmentOne fragmentOne;
+    FragmentTwo fragmentTwo;
+    FragmentThree fragmentThree;
+    FragmentFour fragmentFour;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initData();
         initView();
-        initLisenter();
-    }
-
-
-
-    private void initView() {
-        viewPager = findViewById(R.id.viewpager);
-        bottomNavigationView = findViewById(R.id.navigation);
-        bottomNavigationView.setLabelVisibilityMode(1);
-        bottomNavigationView.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
-        setupViewPager(viewPager);
 
     }
-    private void initLisenter() {
-        viewPager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
-            @Override
-            public void onPageScrolled(int i, float v, int i1) {
-
-            }
-
-            @Override
-            public void onPageSelected(int i) {
-                if (menuItem != null) {
-                    menuItem.setChecked(false);
-                } else {
-                    bottomNavigationView.getMenu().getItem(0).setChecked(false);
-                }
-                menuItem = bottomNavigationView.getMenu().getItem(i);
-                menuItem.setChecked(true);
-            }
-
-            @Override
-            public void onPageScrollStateChanged(int i) {
-
-            }
-        });
-    }
-    private void setupViewPager(ViewPager viewPager) {
-        ViewPagerAdapter adapter = new ViewPagerAdapter(getSupportFragmentManager());
+    private void initData() {
         ArrayList<Integer> images = new ArrayList<Integer>();
         images.add(R.mipmap.banner_one);
         images.add(R.mipmap.banner_two);
         images.add(R.mipmap.banner_three);
         images.add(R.mipmap.banner_four);
-
-        adapter.addFragment(FragmentOne.newInstance(images));
-        adapter.addFragment(FragmentTwo.newInstance("TWO ACTIVITY"));
-        adapter.addFragment(new FragmentThree());
-        adapter.addFragment(new FragmentFour());
-        viewPager.setAdapter(adapter);
+        fragmentOne = FragmentOne.newInstance(images);
+        fragmentTwo=FragmentTwo.newInstance("two");
+        fragmentThree = new FragmentThree();
+        fragmentFour = new FragmentFour();
     }
+    private void initView() {
+        bottomNavigationView = findViewById(R.id.navigation);
+        bottomNavigationView.setLabelVisibilityMode(1);
+        bottomNavigationView.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+    }
+
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
-
         @Override
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+            hideAllFragment(transaction);
             switch (item.getItemId()) {
                 case R.id.navigation_home:
-                    viewPager.setCurrentItem(0);
+                    if(fragmentOne!=null){
+                        transaction.show(fragmentOne);
+                    }else{
+                        transaction.add(R.id.FramePage,fragmentOne);
+                    }
                     return true;
                 case R.id.navigation_dashboard:
-                    viewPager.setCurrentItem(1);
+                    if(fragmentTwo!=null){
+                        transaction.show(fragmentTwo);
+                    }else{
+                        transaction.add(R.id.FramePage,fragmentTwo);
+                    }
                     return true;
                 case R.id.navigation_notifications:
-                    viewPager.setCurrentItem(2);
+                    if(fragmentThree!=null){
+                        transaction.show(fragmentThree);
+                    }else{
+                        transaction.add(R.id.FramePage,fragmentThree);
+                    }
                     return true;
                 case R.id.navigation_donut:
-                    viewPager.setCurrentItem(3);
+                    if(fragmentFour!=null){
+                        transaction.show(fragmentFour);
+                    }else{
+                        transaction.add(R.id.FramePage,fragmentFour);
+                    }
                     return true;
             }
             return false;
         }
     };
 
+    //隐藏所有Fragment
+    public void hideAllFragment(FragmentTransaction transaction){
+        if(fragmentOne!=null){
+            transaction.hide(fragmentOne);
+        }
+        if(fragmentTwo!=null){
+            transaction.hide(fragmentTwo);
+        }
+        if(fragmentThree!=null){
+            transaction.hide(fragmentThree);
+        }
+        if(fragmentFour!=null){
+            transaction.hide(fragmentFour);
+        }
+    }
 }

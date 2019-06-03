633d12d050323863edae378dad4a253f2dffe3f3
syssun <2506715686@qq.com>
2019-06-03 21:32:27 +0800

    pullToRefresh

--- app/src/main/java/com/sys/fragments/FragmentThree.java
+++ app/src/main/java/com/sys/fragments/FragmentThree.java
@@ -8,18 +8,22 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.scwang.smartrefresh.layout.SmartRefreshLayout;
 import com.sys.R;
 import com.sys.activitys.BaseFragment;
 import com.sys.views.LoadingFrame;
 
 public class FragmentThree extends BaseFragment {
      View view;
+    SmartRefreshLayout smartRefreshLayout;
     LoadingFrame frame;
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         view = inflater.inflate(R.layout.fragment_three, container,false);
         initNavBar(view,false,"three",false);
+        smartRefreshLayout = view.findViewById(R.id.refreshLayout);
+        smartRefreshLayout.autoRefresh();
         return view;
     }
 

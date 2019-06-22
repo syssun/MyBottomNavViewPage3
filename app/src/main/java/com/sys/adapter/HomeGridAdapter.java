bdb56dd4f66f01d3554f7dfd38c7b3d08db507e3
syssun <2506715686@qq.com>
2019-06-22 14:53:07 +0800

    okhttp

--- app/src/main/java/com/sys/adapter/HomeGridAdapter.java
+++ app/src/main/java/com/sys/adapter/HomeGridAdapter.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.media.Image;
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -14,7 +15,14 @@ import android.widget.Toast;
 import com.sys.R;
 import com.sys.entitys.HomeGrid;
 
+import java.io.IOException;
 import java.util.ArrayList;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 
 public class HomeGridAdapter extends RecyclerView.Adapter<HomeGridAdapter.ViewHoler> {
     Context context;
@@ -39,10 +47,35 @@ public class HomeGridAdapter extends RecyclerView.Adapter<HomeGridAdapter.ViewHo
         viewHoler.itemView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                doGet();
                 Toast.makeText(context,homeGrid.getTitle(),Toast.LENGTH_SHORT).show();
+
             }
         });
     }
+
+    public void doGet(){
+        String url = "http://wwww.baidu.com";
+        OkHttpClient okHttpClient = new OkHttpClient();
+        final Request request = new Request.Builder()
+                .url(url)
+                .get()//默认就是GET请求，可以不写
+                .build();
+        Call call = okHttpClient.newCall(request);
+        call.enqueue(new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+                Log.d("dogetonfailus", "onFailure: ");
+                Log.getStackTraceString(e);
+            }
+
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+                Log.d("doget", "onResponse: " + response.body().string());
+            }
+        });
+    }
+
 
     @Override
     public int getItemCount() {
